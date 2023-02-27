a0fc0ae2c5ed8e4aa1e36303cd9843e9aaae9f65
emlez <emilio@trynow.io>
2023-02-26 23:21:41 -0600

    fix: Export TreeNode class to reuse it

--- bytes/trees/findValue.ts
+++ bytes/trees/findValue.ts
@@ -22,16 +22,7 @@
 //       6   9
 // and the search value 7 return null.
 
-class TreeNode {
-  val: number
-  left: TreeNode | null
-  right: TreeNode | null
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val
-    this.left = left === undefined ? null : left
-    this.right = right === undefined ? null : right
-  }
-}
+import { TreeNode } from "./treeNode"
 
 const findValue = (root: TreeNode | null, value: number): TreeNode | null => {
   if (!root) return null
